a3f2c60934491b936d8949120e205f31668c0610
Cfp <cfp@cfp.gotdns.ch>
2022-01-29 20:28:42 +0100

    Do some config stuff

---
+++ README.md
@@ -0,0 +1,27 @@
+# NeoPrint
+
+Print images in your terminal!
+
+## Works on
+
+Ubuntu
+
+Tell me if it works on your OS!
+
+## Installing
+
+1. Clone the repo: `git clone https://github.com/cfpwastaken/neoprint` or `gh repo clone cfpwastaken/neoprint` if you use the github cli
+2. CD into the directory: `cd neoprint`
+3. Install the Dependencies: `npm i`
+4. Link neoprint: `npm link`
+
+## Usage
+
+Just use `nprint file.png`
+
+### Optional Arguments
+
+You can set the size or width/height like this:
+
+`nprint file.png --size 5` (higher is smaller)
+`nprint file.png --width 69 --height 69`
--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
-import terminalKitPKG from "terminal-kit";
-const { terminal: term } = terminalKitPKG;
-import { existsSync } from "fs";
+#!/usr/bin/env node
+const term = require("terminal-kit").terminal;
+const fs = require("fs");
 
-const path = process.argv[1];
+const path = process.argv[2];
 
-if(existsSync(path)) {
-  term.drawImage(path);
+function termArg(arg: string, def: string) {
+  return process.argv.includes("--" + arg) ? process.argv[process.argv.indexOf("--" + arg) + 1] : def;
+}
+
+if(fs.existsSync(path)) {
+  const size = parseInt(termArg("size", "-1"));
+  const width = parseInt(termArg("width", term.width));
+  const height = parseInt(termArg("height", (term.height * (size == -1 ? 2 : size)) + "" ));
+  term.drawImage(path, { shrink: { width, height } });
 } else {
-  term("Not found!")
+  term.red("Not found!\n")
 }
 
